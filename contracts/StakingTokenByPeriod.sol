5bfcd85bd75d36848d3df5c505b33bdaaf4ca264
Utilisateur <fidelity@it-tecos.fr>
2021-07-13 17:52:17 +0200

    Smart contract with staking rewards and echlons

---
+++ contracts/StakingTokenByPeriod.sol
@@ -0,0 +1,307 @@
+pragma solidity ^0.8.0;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/utils/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
+
+/**
+ * @title Fidelity Token smart contract
+ * @author TECOS
+ * @notice Implements a basic ERC20 staking Fidelity Token.
+ */
+contract StakingTokenByPeriod is ERC20, Ownable {
+    using SafeMath for uint256;
+
+    // *-*-*-*-*-*-*_Attibutes_*-*-*-*-*-*-*
+
+    // struct EchelonReward is used to build rewards echelons
+    struct EchelonReward {
+        uint256 echelonMaxValue;
+        uint256 percentage;
+    }
+
+    // stakeholders array
+    address[] internal stakeholders;
+
+    // minimum balance value since the last rewards distribution
+    mapping(address => uint256) internal rewardableBalance;
+
+    // timestamp to store when the next reward distribution will be possible
+    uint256 internal nextRewardsAvailableTime;
+
+    // duration between each reward distribution
+    uint256 internal rewardsDuration = 2 minutes;
+
+    // reward percentage by tokens
+    EchelonReward[] internal rewardsPercentageByTokens;
+
+
+    // *-*-*-*-*-*-*_Constructor_*-*-*-*-*-*-*
+
+    /**
+     * @notice Constructor for smart contract creation
+    */
+    constructor() ERC20("FidelityToken", "FT") {
+        _mint(msg.sender, 500 * 10 ** uint256(decimals()));
+        fillRewardsPercentageByTokensOnStart();
+        nextRewardsAvailableTime = block.timestamp.add(rewardsDuration);
+    }
+
+
+    // *-*-*-*-*-*-*_Stakeholders_*-*-*-*-*-*-*
+
+    /**
+     * @notice A method to check if an address is a stakeholder.
+     * @param _address The address to verify.
+     * @return bool, uint256 Whether the address is a stakeholder, 
+     * and if so its position in the stakeholders array.
+    */
+    function isStakeholder(address _address)
+        public
+        view
+        returns(bool, uint256)
+    {
+        for (uint256 s = 0; s < stakeholders.length; s += 1){
+            if (_address == stakeholders[s]) return (true, s);
+        }
+        return (false, 0);
+    }
+
+    /**
+     * @notice A method to add a stakeholder.
+     * @param _stakeholder The stakeholder to add.
+    */
+    function addStakeholder(address _stakeholder)
+        public
+    {
+        (bool _isStakeholder, ) = isStakeholder(_stakeholder);
+        if(!_isStakeholder) {
+            stakeholders.push(_stakeholder);
+            rewardableBalance[_stakeholder] = 0;
+        }
+    }
+
+    /**
+     * @notice A method to remove a stakeholder.
+     * @param _stakeholder The stakeholder to remove.
+    */
+    function removeStakeholder(address _stakeholder)
+        public
+    {
+        (bool _isStakeholder, uint256 s) = isStakeholder(_stakeholder);
+        if(_isStakeholder){
+            stakeholders[s] = stakeholders[stakeholders.length - 1];
+            stakeholders.pop();
+            rewardableBalance[_stakeholder] = 0;
+        } 
+    }
+
+
+    // *-*-*-*-*-*-*_Rewards_*-*-*-*-*-*-*
+
+    /** 
+     * @notice A simple method that calculates the rewards of the given stakeholder.
+     * @param _stakeholder The stakeholder to calculate rewards for.
+    */
+    function calculateReward(address _stakeholder)
+        public
+        view
+        returns(uint256)
+    {
+        return getRewardableBalanceByStakeholder(_stakeholder) * getRewardPercentage(_stakeholder) / 100;
+    }
+
+    /**
+     * @notice A method to get the rewardable balance of the given stakeholder.
+     * @param _stakeholder The stakeholder.
+     * @return The amount of the rewardable tokens of the given stakeholder
+    */
+    function getRewardableBalanceByStakeholder(address _stakeholder)
+        public
+        view
+        returns(uint256)
+    {
+        return rewardableBalance[_stakeholder];
+    }
+
+    /**
+     * @notice A method to get the reward percentage by balance.
+     * @param _stakeholder The stakeholder.
+     * @return The reward percentage according to the stakeholder's balance
+    */
+    function getRewardPercentage(address _stakeholder)
+        public
+        view
+        returns(uint256)
+    {
+        if (0 != rewardsPercentageByTokens.length) {
+            for (uint index = rewardsPercentageByTokens.length; index > 0; index--) {
+                if (rewardableBalance[_stakeholder] >= rewardsPercentageByTokens[index - 1].echelonMaxValue) {
+                    return rewardsPercentageByTokens[index - 1].percentage;
+                }
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * @notice A method to get the reward percentage by balance.
+     * @return The reward percentage according to the stakeholder's balance
+    */
+    function getRewardsPercentageByTokens() public view returns (EchelonReward[] memory) {
+        return rewardsPercentageByTokens;
+    }
+
+    /**
+     * @notice A method to distribute rewards to all stakeholders.
+    */
+    function distributeRewards() 
+        public
+        onlyOwner
+    {
+        for (uint256 s = 0; s < stakeholders.length; s += 1){
+            address stakeholder = stakeholders[s];
+            if (block.timestamp > nextRewardsAvailableTime) {
+                updateRewardableBalancePerAddress(stakeholder);
+                uint256 reward = calculateReward(stakeholder);
+                increaseAllowance(owner(), reward);
+                ERC20.transferFrom(owner(), stakeholder, reward);
+                rewardableBalance[stakeholder] = balanceOf(stakeholder);
+            }
+        }
+        nextRewardsAvailableTime = block.timestamp.add(rewardsDuration);
+    }
+
+    /**
+     * @notice A method to update the rewards percentage echlons.
+     * @param newEchlonArray The new rewards echlons.
+    */
+    function updateRewardsPercentageByTokens(EchelonReward[] calldata newEchlonArray) public onlyOwner{
+        if (0 != newEchlonArray.length) {
+            delete rewardsPercentageByTokens;
+            for (uint index = 0; index < newEchlonArray.length; index++) {
+                EchelonReward memory echelon;
+                echelon.echelonMaxValue = newEchlonArray[index].echelonMaxValue * 10 ** uint256(decimals());
+                echelon.percentage = newEchlonArray[index].percentage;
+                rewardsPercentageByTokens[index] = echelon;
+            }
+        }
+    }
+
+    /**
+     * @notice A method to initialize the reward percntages.
+    */
+    function fillRewardsPercentageByTokensOnStart() private {
+        rewardsPercentageByTokens.push(EchelonReward(
+            {
+                echelonMaxValue : 100 * 10 ** uint256(decimals()),
+                percentage : 1
+            }));
+
+        rewardsPercentageByTokens.push(EchelonReward(
+            {
+                echelonMaxValue : 500 * 10 ** uint256(decimals()),
+                percentage : 2
+            }));
+
+        rewardsPercentageByTokens.push(EchelonReward(
+            {
+                echelonMaxValue : 1000 * 10 ** uint256(decimals()),
+                percentage : 3
+            }));
+
+        rewardsPercentageByTokens.push(EchelonReward(
+            {
+                echelonMaxValue : 10000 * 10 ** uint256(decimals()),
+                percentage : 4
+            }));
+        
+        rewardsPercentageByTokens.push(EchelonReward(
+            {
+                echelonMaxValue : 50000 * 10 ** uint256(decimals()),
+                percentage : 5
+            }));
+    }
+
+    /**
+     * @notice A method to update the rewardable balance for the given stakeholder.
+     * @param _stakeholder The stakeholder.
+    */
+    function updateRewardableBalancePerAddress(address _stakeholder) public onlyOwner {
+        if (balanceOf(_stakeholder) < rewardableBalance[_stakeholder]) {
+            rewardableBalance[_stakeholder] = balanceOf(_stakeholder);
+        }
+    }
+
+    /**
+     * @notice A method to update the rewardable balance for all the stakeholders.
+    */
+    function updateRewardableBalances() public onlyOwner {
+        for (uint index = 0; index < stakeholders.length; index++) {
+            updateRewardableBalancePerAddress(stakeholders[index]);
+        } 
+    }
+
+
+    // *-*-*-*-*-*-*_Transactions_*-*-*-*-*-*-*
+
+    /**
+     * @notice A method to send tokens from the retailer to the customer.
+     * @param retailer The retailer address.
+     * @param customer The customer address.
+     * @param tokens The amount of tokens to send.
+    */
+    function sendTokensFromRetailerToCustomer(address retailer, address customer, uint256 tokens) public {
+        increaseAllowance(retailer, tokens * 10 ** uint256(decimals()));
+        ERC20.transferFrom(retailer, customer, tokens * 10 ** uint256(decimals()));
+        updateRewardableBalancePerAddress(customer);
+        updateRewardableBalancePerAddress(retailer);
+    }
+
+    /**
+     * @notice A method to send tokens from the customer to the retailer.
+     * @param retailer The retailer address.
+     * @param customer The customer address.
+     * @param tokens The amount of tokens to send.
+    */
+    function sendTokensFromCustomerToRetailer(address retailer, address customer, uint256 tokens) public {
+        increaseAllowance(customer, tokens * 10 ** uint256(decimals()));
+        ERC20.transferFrom(customer, retailer, tokens * 10 ** uint256(decimals()));
+        updateRewardableBalancePerAddress(customer);
+        updateRewardableBalancePerAddress(retailer);
+    }
+
+    /**
+     * @notice A method to enable customers to send tokens from one retailer to another.
+     * @param retailerSource The retailer source address.
+     * @param walletSource The customer address in the source retailer.
+     * @param walletDestination The customer address in the target retailer.
+     * @param tokensToSend The amount of tokens to send.
+     * @param feesForFidelityPercent The percentage of transaction fees for Fidelity.
+     * @param feesForRetailerSourcePercent The percentage of transaction fees for the source retailer.
+    */
+    function stansfertFromRetailerToAnother(address retailerSource, address walletSource, address walletDestination, uint256 tokensToSend,
+            uint256 feesForFidelityPercent, uint256 feesForRetailerSourcePercent) public {
+                
+        uint256 feesForFidelity = (tokensToSend * feesForFidelityPercent / 100) * 10 ** uint256(decimals());
+        uint256 feesForRetailerSource = (tokensToSend * feesForRetailerSourcePercent / 100) * 10 ** uint256(decimals());
+        
+        increaseAllowance(walletSource, tokensToSend * 10 ** uint256(decimals()));
+        ERC20.transferFrom(walletSource, walletDestination, (tokensToSend * 10 ** uint256(decimals())) - feesForFidelity - feesForRetailerSource);
+        ERC20.transferFrom(walletSource, retailerSource, feesForRetailerSource);
+        ERC20.transferFrom(walletSource, owner(), feesForFidelity);
+        
+        updateRewardableBalancePerAddress(retailerSource);
+        updateRewardableBalancePerAddress(walletSource);
+        updateRewardableBalancePerAddress(walletDestination);
+        updateRewardableBalancePerAddress(owner());
+    }
+
+    /**
+     * @notice A method to min extra tokens.
+     * @param tokens The amount of tokens to mint.
+    */
+    function mintExtraTokens(uint256 tokens) public {
+        _mint(owner(), tokens * 10 ** uint256(decimals()));
+    }
+}
